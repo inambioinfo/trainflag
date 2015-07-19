85c51bdfbed153b803d6b8d6f338a12f9ea4605a
Simon Andrews <simon@proteo.me.uk>
2015-07-19 13:30:19 +0100

    Added heatbeat check to the server for dead clients.

---
+++ uk/ac/babraham/trainflag/server/network/ClientHeartbeatCheck.java
@@ -0,0 +1,51 @@
+package uk.ac.babraham.trainflag.server.network;
+
+import java.io.IOException;
+import java.net.InetAddress;
+
+import uk.ac.babraham.trainflag.server.ClientInstance;
+import uk.ac.babraham.trainflag.server.ClientSet;
+
+public class ClientHeartbeatCheck implements Runnable {
+
+	private ClientSet clients;
+
+	public ClientHeartbeatCheck (ClientSet clients) {
+		this.clients = clients;
+		Thread t = new Thread(this);
+		t.start();
+	}
+
+	public void run() {
+
+		while (true) {
+
+			ClientInstance [] clients = this.clients.clients();
+
+			for (int c=0;c<clients.length;c++) {
+
+				try {
+					if (! ClientInstaceForServer.sendCommand(new String [] {"PING"}, new InetAddress [] {clients[c].address()})[0]) {
+						this.clients.removeClient(clients[c]);
+					}
+				}
+				catch (IOException ioe) {
+					this.clients.removeClient(clients[c]);
+					}
+			}
+
+
+			try {
+				Thread.sleep(10000);
+			} 
+			catch (InterruptedException e) {}
+
+		}
+
+	}
+
+
+
+
+
+}
